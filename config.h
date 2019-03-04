ec30fc6f457109460add55787dfa7ac2871884e8
Roguepullrequest <Nunya@thisisforspam.com>
2019-03-04 03:16:53 +0000

    Inital commit: added the basic files needed for planck keyboard.

---
+++ config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define AUDIO_CLICKY
+
+#ifdef AUDIO_ENABLE
+	#define STARTUP_SONG SONG(TO_BOLDLY_GO)
+	#define DEFAULT_LAYER_SONGS { \
+		SONG(ZELDA_PUZZLE),   \
+	       	SONG(MARIO_MUSHROOM), \
+		SONG(MARIO_GAMEOVER), \
+		SONG(RICK_ROLL)       \
+	}
