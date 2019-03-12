7a84f14ac17d7a76d72b9210f37fdf3a2bec4828
Roguepullrequest <Nunya@thisisforspam.com>
2019-03-12 06:35:00 +0000

    LAST still doesn't work but we are trying

--- config.h
+++ config.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#define AUDIO_CLICKY
-
 #ifdef AUDIO_ENABLE
 	#define STARTUP_SONG SONG(TO_BOLDLY_GO)
 	#define DEFAULT_LAYER_SONGS { \
-		SONG(ZELDA_PUZZLE),   \
+/*		SONG(ZELDA_PUZZLE),   \
 	       	SONG(MARIO_MUSHROOM), \
 		SONG(MARIO_GAMEOVER), \
 		SONG(RICK_ROLL)       \
-	}
+*/	}
 #endif
